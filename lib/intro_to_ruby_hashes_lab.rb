6b5359e22e2467530550aff85c3ed767c435016d
Ward Price <wardprice@gmail.com>
2020-06-27 19:46:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ end
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
   has = {
-    :name 'Grace Hopper'
+    :name => 'Grace Hopper'
   }
 end
 
@@ -19,6 +19,6 @@ end
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
   hash = {
-    :id number
+    :id => number
   }
 end
