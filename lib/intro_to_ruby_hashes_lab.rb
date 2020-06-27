062bf4647fea22f946c08ead1080c724c4a9142a
Ward Price <wardprice@gmail.com>
2020-06-27 19:46:13 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,24 @@
 def new_hash
-  # return an empty hash
+  hash = Hash.new
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  hash = {
+    example: "there you go"
+  }
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  has = {
+    :name => 'Grace Hopper'
+  }
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  hash = {
+    :id => number
+  }
 end
